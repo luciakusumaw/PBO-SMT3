12d3a8371dd0481dabf9f3a86b4746d78cd2f068
luciakusumaw <itsmelucia27@gmail.com>
2026-09-01 17:38:24 +0700

    Revisied

--- Jobsheet2/Hotel.java
+++ Jobsheet2/Hotel.java
@@ -1,9 +1,10 @@
 package Jobsheet2;
 
 public class Hotel {
-    public String guestName;
+    public String guestName, roomNumber, bedType;
     public int duration;
     public double price;
+    public boolean isClean;
 
     public void bookRoom(){
         System.out.println("Booking is sucess for " + guestName+ " for " +duration+" nights");
@@ -13,11 +14,22 @@ public class Hotel {
         return price*duration;
     }
 
+    public void cleanRoom(boolean isClean) {
+        this.isClean = isClean;
+        if (this.isClean) {
+            System.out.println("Room " + roomNumber + " is ready");
+        } else {
+            System.out.println("Room " + roomNumber + " is dirty and needs cleaning\n");
+        }
+    }
+
     public void displayInfo(){
         System.out.println("Guest Name: " + guestName);
         System.out.println("Duration: " + duration + " nights");
         System.out.println("Room Price: Rp " + price);
+        System.out.println("Room Type: "+bedType);
+        System.out.println("Room status: "+ (isClean ? "Clean" : "Needs Cleaning"));
         System.out.println("Total Paid: Rp " + calculateTotalPayment());
-
+    
     }
 }
--- Jobsheet2/HotelDemo.java
+++ Jobsheet2/HotelDemo.java
@@ -6,16 +6,25 @@ public class HotelDemo {
         guest1.guestName = "Andi";
         guest1.duration = 2;
         guest1.price = 500000;
+        guest1.bedType = "Deluxe";
+        guest1.roomNumber = "101";
+        guest1.cleanRoom(true);
 
         Hotel guest2 = new Hotel();
         guest2.guestName = "Lucia";
         guest2.duration = 3;
         guest2.price = 750000;
+        guest2.bedType = "Superior";   
+        guest2.roomNumber = "201";     
+        guest2.cleanRoom(false);
 
         guest1.displayInfo();
         guest1.bookRoom();
+        System.out.println();
+
         guest2.displayInfo();
         guest2.bookRoom();
+        System.out.println();
 
         System.out.println("Updating guest details\n");
 
